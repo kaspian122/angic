9c302eb8d84f535386f9d7989b8a46394e8d1df9
Anton Zheltov <zheltovad@sib-soft.ru>
2018-04-04 19:28:14 +0700

    MKD-28 убрал еще хлам

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
-
 import {AppComponent} from './components/app/app.component';
 import {LoginComponent} from './components/login/login.component';
 import {UserComponent} from "./components/user/user.component";
