db61de3f87616b16aa2dd2b4f5f5f5e3f679547d
Anton Zheltov <anton6378@gmail.com>
2018-04-09 04:27:18 +0700

    MKD-28 каркас приложения

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './components/app/app.component';
 import {LoginComponent} from './components/login/login.component';
@@ -14,7 +15,15 @@ import {CookieService} from "ngx-cookie-service";
 import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import {HttpAuthInterceptor} from "./services/auth/http.auth.interceptor";
 import {ForbiddenComponent} from "./components/forbidden/forbidden.component";
-
+import { CovalentLayoutModule } from '@covalent/core/layout';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { ProfileComponent } from './components/main/profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -22,13 +31,23 @@ import {ForbiddenComponent} from "./components/forbidden/forbidden.component";
     LoginComponent,
     UserComponent,
     MainComponent,
-    ForbiddenComponent
+    ForbiddenComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
-    routing
+    routing,
+    CovalentLayoutModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatSidenavModule,
   ],
   providers: [
     CookieService,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,17 +4,24 @@ import {LoginComponent} from "./components/login/login.component";
 import {MainComponent} from "./components/main/main.component";
 import {UserComponent} from "./components/user/user.component";
 import {ForbiddenComponent} from "./components/forbidden/forbidden.component";
+import {ProfileComponent} from "./components/main/profile/profile.component";
 
 const routes: Routes = [
   {
     path: '',
-    canActivate: [AuthGuard],
+    // canActivate: [AuthGuard],
     pathMatch: 'full',
     component: UserComponent,
     children: [
       {
         path: '',
         component: MainComponent,
+        children: [
+          {
+            path: '',
+            component: ProfileComponent,
+          }
+        ]
       }
     ]
   },
