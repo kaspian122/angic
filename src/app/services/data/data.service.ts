25013c2a4164354bd435ccbf942aafca5558b3e0
Elena Barkhatova <barkhatovaea@sib-soft.ru>
2018-04-12 14:54:22 +0700

    MKD-42 add mkd list

--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -7,8 +7,6 @@ import {Observable} from "rxjs/Observable";
 @Injectable()
 export class DataService {
 
-  private mkd?: Mkd[] = [];
-
   constructor(
     private http: HttpClient,
     private config: AppConfig,
@@ -16,19 +14,8 @@ export class DataService {
 
   public getMkdList(): Promise<Mkd[]>{
     let response: Observable<Mkd[]> = this.http.request<Mkd[]>("GET", this.config.getEndpoint("mkd"));
+    return response.toPromise<Mkd[]>()
 
-    return response
-      .toPromise<Mkd[]>()
-      .then(
-        it => {
-          this.mkd = it;
-          return it;
-        },
-        err => {
-          this.mkd = null;
-          throw err;
-        }
-      );
   }
 
 
